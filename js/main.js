9203c86c187a8d5deedf11df5472cf563a536a1c
Chanala Wilangka <c.wilangka@gmail.com>
2016-10-05 14:01:17 +0700

    init slide content

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
-   
-
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-                menu: '#menu',
-                anchors: ['firstPage', 'secondPage', '3rdPage'],
-                sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-                autoScrolling: false
-            });
-        });
 
 
+
+    $('#fullpage').fullpage({
+        menu: '#menu',
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        sectionsColor: ['#fff', '#1BBC9B', '#7E8F7C', '#1BBC9B', '#7E8F7C'],
+        autoScrolling: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        navContainer: '#customNav',
+        loop: true,
+        margin: 10,
+        nav: false,
+        mouseDrag: false,
+        touchDrag: false,
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    })
 });
-
